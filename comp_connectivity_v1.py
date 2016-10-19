a15ddf88b5de0c379bc462e967511f7648822470
wronk <wronk.mark@gmail.com>
2016-10-19 11:05:52 -0700

    Various fixes and updates

--- comp_connectivity_v1.py
+++ comp_connectivity_v1.py
@@ -4,6 +4,12 @@ comp_connectivity_v1.py
 @author: wronk
 
 Compute connectivity metrics between RSN areas
+
+Handling voc_meg data: This code takes all auditory trials and stitchs them
+together into one long time-series before computing correlations. This isn't
+ideal as the time series will often jump to the start of the next trial.
+
+Output data is saved as (n_ROIs, n_freqs, n_times).
 """
 
 import os
@@ -13,130 +19,202 @@ import cPickle
 from copy import deepcopy
 
 import mne
-from mne import read_epochs, read_source_spaces
-from mne.minimum_norm import apply_inverse_epochs as apply_inv, read_inverse_operator as read_inv
+from mne import (read_epochs, read_source_spaces,
+                 extract_label_time_course as eltc)
+from mne.minimum_norm import (apply_inverse_epochs as apply_inv,
+                              read_inverse_operator as read_inv)
 from mne.connectivity import spectral_connectivity as conn
 from mne.label import read_label
+from mne.time_frequency.tfr import _compute_tfr
+from pandas import rolling_corr
+
+from config import config_conn_methods, config_conn_params
 
 ################
 # Define globals
 ################
-save_data = False  # Whether or not to pickle data
+save_data = True  # Whether or not to pickle data
 
-data_dir = op.join(os.environ['CODE_ROOT'], 'rsn_data')
 struct_dir = os.environ['SUBJECTS_DIR']
+data_head = op.join(os.environ['CODE_ROOT'])
 
-subj_nums = ['004', '007', '015', '017', '019', '020', '023', '031', '032',
-             '034', '038']
-subj_nums = ['017']
+# Choose to process resting data or task data
+#exp_heading = 'wronk_resting'
+exp_heading = 'eric_voc'
 
 
-# TODO: check labels for each subject, not all have the same number of
-# divisions
+if exp_heading == 'wronk_resting':
+    data_dir = op.join(data_head, 'rsn_data')
+else:
+    data_dir = op.join(data_head, 'voc_data')
+    trial_types = ['M10_', 'M20_', 'S10_', 'S20_']
 
-# Divisions on fsaverage
-# div1: MPFC (Medial frontal region)
-# div2: STS (Superior temporal sulcus)
-# div3: PCC, pCun (Posterior cing. cort., preCuneus)
-# div4: TPJ, (temporoparietal junction)
-# div5: XXX Name? Small region on medial/ventral side of temporal lobe
+subj_nums = ['04', '07', '15', '17', '19', '20', '23', '31', '32', '34', '38']
+subj_nums = ['15']
 
-# Define processing parameters
-common_conn_kwargs = dict(fmin=2., fmax=50., fskip=4)
-conn_methods = [dict(method='coh').update(common_conn_kwargs),
-                dict(method='pli').update(common_conn_kwargs)]
+conn_methods = config_conn_methods
+conn_params = config_conn_params
+conn_params.update(dict(subj_nums=['0%s' % s_num for s_num in subj_nums]))
 
-conn_params = dict(conn_pairs=[(2, 0), (2, 4)],
-                   conn_methods=conn_methods,
-                   rsn_labels=['lh.7Networks_7_div%i.label' % i
-                               for i in range(1, 6)],
-                   subj_nums=subj_nums,
-                   mean_mode='mean_flip',
-                   n_jobs=6)
+inv_lam = 1. / 9
 
 
 ##################
 # Helper functions
 ##################
-def get_lab_list(conn_params, s_name):
-    """Helper to get list of labels and a summed master label"""
+def get_lab_list(conn_params, s_name, src):
+    """Helper to get list of labels and all vertices"""
 
     assert('rsn_labels' in conn_params.keys())
     print 'Loading %i labels for subject: %s' % \
         (len(conn_params['rsn_labels']), s_name)
 
     lab_list = []
+    lab_vert_list = []
 
     for label_name in conn_params['rsn_labels']:
+        # Load label, get vertices
         fname_label = op.join(struct_dir, s_name, 'label', label_name)
-        lab_list.append(read_label(fname_label, subject=s_name))
-        #summed_label += lab_list[-1]
+        temp_label = read_label(fname_label, subject=s_name)
 
-    summed_label = deepcopy(lab_list[0])
-    for temp_lab in lab_list[1:]:
-        summed_label += temp_lab
+        lab_list.append(temp_label)
+        lab_vert_list.extend(list(lab_list[-1].vertices))
 
-    return lab_list, summed_label
+    return lab_list, lab_vert_list
+
+
+def tfr_split(label_activity):
+    """Helper to calculate wavelet power in batches instead of all at once
+
+    Needed for memory issues.
+    """
+    batch_size = 2
+    batch_list = []
+
+    power_arr = np.zeros((label_activity.shape[0], label_activity.shape[1],
+                          len(conn_dict['cwt_frequencies']),
+                          label_activity.shape[2]))
+
+    batch_inds = range(0, label_activity.shape[0], batch_size)
+    batch_inds.append(label_activity.shape[0])
+
+    for bi1, bi2 in zip(batch_inds[:-1], batch_inds[1:]):
+
+        batch = _compute_tfr(label_activity[bi1:bi2],
+                             frequencies=conn_dict['cwt_frequencies'],
+                             sfreq=epo.info['sfreq'],
+                             n_cycles=conn_dict['n_cycles'],
+                             n_jobs=1, output='power')
+        power_arr[bi1:bi2, :, :, :] = batch
+
+    return power_arr
 
 ###########################################################
-# Compute connectivity between RSN regions for each subject
+# Loop through each subject; load info, comp connectivity
 ###########################################################
 
-for s_num in conn_params['subj_nums']:
-    s_name = 'AKCLEE_' + s_num
-    struct_name = 'AKCLEE_1' + s_num[-2:]
+for s_num in subj_nums:
+    num_exp = '0%s' % s_num
+    s_name = 'AKCLEE_1' + s_num
+    print '\nProcessing: ' + s_name
 
+    print '\n\tLoading epo, inv, src, labels'
     # Load Epochs
-    fname_epo = op.join(data_dir, 'wronk_resting_%s' % s_num, 'epochs',
-                        'All_55-sss_wronk_resting_%s-epo.fif' % s_num)
-    epo = read_epochs(fname_epo)
+    epo = read_epochs(op.join(data_dir, '%s_%s' % (exp_heading, num_exp),
+                              'epochs', 'All_55-sss_%s_%s-epo.fif' %
+                              (exp_heading, num_exp)))
 
-    # Get list of labels and summed label to facilitate source estimation
-    lab_list, summed_label = get_lab_list(conn_params, struct_name)
+    # Subselect only vocoder trials (and throw away visual control trials)
+    if exp_heading == 'voc_meg':
+        epo = epo[trial_types]
+        #epo.crop(None, 5.2)  # Crop to end at last stimulus letter
 
     # Generate source activity restricted to pre-defined RSN areas
-    fname_inv = op.join(data_dir, 'wronk_resting_%s' % s_num, 'inverse',
-                        'wronk_resting_%s-55-sss-meg-erm-fixed-inv.fif' % s_num)
+    # XXX Using MEG only ERM inverse
+    fname_inv = op.join(data_head, 'voc_data', 'eric_voc_%s' % num_exp,
+                        'inverse', 'eric_voc_%s-55-sss-meg-erm-fixed-inv.fif' %
+                        num_exp)
     inv = read_inv(fname_inv)
-    stc = apply_inv(epo, inv, lambda2=1. / 9., label=summed_label,
-                    method='MNE')
 
-    # XXX: Could morph to fsaverage if we want to
-    src = read_source_spaces(op.join(struct_dir, struct_name, 'bem',
-                                     struct_name + '-7-src.fif'),
-                             verbose=False)
+    # XXX: 'src' object in structs_dir doesn't match src file in `struct_dir`
+    src = inv['src']
 
-    # Compute connectivity
-    conn_results = []
-    for label_pair in conn_params['conn_pairs']:
-        import ipdb; ipdb.set_trace()
-        traces = [mne.extract_label_time_course(stc[0], lab_list[li], src,
-                                                mode=conn_params['mean_mode'])
-                  for li in label_pair]
+    print '\n\tLoading RSN label info'
+    # Get list of labels and vertices
+    lab_list, lab_verts = get_lab_list(conn_params, s_name, src)
+    lab_verts_restricted = np.intersect1d(lab_verts, src[0]['vertno'])
+    lab_verts_restricted.sort()
 
-        for conn_dict in conn_params['conn_methods']:
-            temp_conn = conn(np.array(traces),
-                             indicies=(np.array([0]), np.array([1])),
-                             sfreq=epo.info['sfreq'], n_jobs=params['n_jobs'],
-                             **conn_dict)
+    summed_label = mne.Label(lab_verts_restricted, hemi='lh',
+                             name='RSNs_summed', subject=s_name)
 
-            conn_results.append(temp_conn)
+    print '\n\tComputing STCs'
+    stc_list = apply_inv(epo, inv, lambda2=inv_lam, method='MNE')
+    # XXX downstream problems when trying to use only labels of interest eltc
+    # requires that n_vertices match between src and stc
+    #stc_list = apply_inv(epo, inv, lambda2=inv_lam, label=summed_label,
+    #                     method='MNE')
+
+    # Extract label time course (n_stcs x n_label x n_times)
+    label_activity = np.array(eltc(stc_list, lab_list, src,
+                                   mode=conn_params['mean_mode'],
+                                   verbose=False))
+
+    import ipdb; ipdb.set_trace()
+    ###########################################################
+    # Compute connectivity between RSN regions for each subject
+    ###########################################################
+    print '\tComputing connectivity'
+    conn_data = []
+    power_data = []
+    for conn_dict in conn_params['conn_methods']:
+        # Sliding correlation of BLP
+        # Compute TFR of power bands
+            # wavelet_ts is (n_stc, n_lab, n_freq, n_times)
+        wavelet_ts = tfr_split(label_activity)
+
+        # Reshape to concatenate data from all epochs into one long time trace
+        wavelet_ts = wavelet_ts.reshape(1, label_activity.shape[1],
+                                        len(conn_dict['cwt_frequencies']),
+                                        (label_activity.shape[0] *
+                                            label_activity.shape[2]))
+        power_data.append(wavelet_ts)
+
+        # Calculate sliding correlation window using pandas.rolling_corr
+        corr_len = int(conn_dict['corr_wind'] * epo.info['sfreq'])
+        blp_corr = np.zeros((len(conn_params['conn_pairs'][0]),
+                             wavelet_ts.shape[2],
+                             wavelet_ts.shape[3]))
+
+        for match_i, (li_1, li_2) in enumerate(zip(conn_params['conn_pairs'][0],
+                                                   conn_params['conn_pairs'][1])):
+            for bp_i in range(wavelet_ts.shape[2]):
+                blp_corr[match_i, bp_i] = \
+                    rolling_corr(wavelet_ts[0, li_1, bp_i, :],
+                                 wavelet_ts[0, li_2, bp_i, :], window=corr_len)
+
+        conn_data.append(blp_corr)  # Store only connectivity traces
 
     ##############
     # Save Results
     ##############
+    print '\tSaving connectivity data'
     if save_data:
 
         # Check if save directory exists (and create it if necessary)
-        save_dir = op.join(data_dir, 'wronk_resting_%s' % s_num,
+        save_dir = op.join(data_dir, '%s_%s' % (exp_heading, num_exp),
                            'connectivity')
         if not os.path.isdir(save_dir):
             os.mkdir(save_dir)
 
         # Save results as pkl file
-        save_file = op.join(save_dir, 'conn_results.pkl')
+        save_file = op.join(save_dir, 'conn_results_%s.pkl' % exp_heading)
         with open(save_file, 'wb') as pkl_obj:
             results_to_save = deepcopy(conn_params)
-            results_to_save['conn_results'] = conn_results
+            results_to_save['conn_data'] = conn_data
+            #results_to_save['power_data'] = power_data
+            results_to_save['conn_data_shape'] = \
+                'n_label_pairs, n_freqs, n_times'
+            results_to_save['sfreq'] = epo.info['sfreq']
 
             cPickle.dump(results_to_save, pkl_obj)
--- preprocess_raw.py
+++ preprocess_raw.py
@@ -9,6 +9,7 @@ import os
 from os import path as op
 import numpy as np
 
+import mne
 import mnefun
 
 # use niprove for handling events if possible
@@ -19,88 +20,112 @@ except ImportError:
     handler = None
 
 data_dir = op.join(os.environ['CODE_ROOT'], 'rsn_data')
-subj_nums = ['004', '007', '015', '017', '019', '020', '023', '031', '032',
-             '034', '038']
-subj_nums = ['017']
+# 019 had bounced 1 triggers in original experiment.
+# 020 had bad pupillometry, and the first run had a TTL issue
+# 007 had bad pupillometry; All epochs rejected
+# 022, 004 had droopy pupillometry
+subj_nums = [15, 17, 19, 23, 31, 32, 34, 38]
+subj_nums = [17]
+epo_len = 1
+debug = False
 
 #######################################
 # Construct subject information
 #######################################
 
-'''
+data_dir = '/media/Toshiba/Code/rsn_data/'
+
 # Create a cropped version of data just for faster debugging
-fname_raw = '/media/Toshiba/Code/rsn_data/wronk_resting_017/raw_fif/wronk_resting_017_01_raw.fif'
-fname_raw_save = '/media/Toshiba/Code/rsn_data/wronk_resting_017/raw_fif/wronk_resting_017_01_short_raw.fif'
-fname_erm = '/media/Toshiba/Code/rsn_data/wronk_resting_017/raw_fif/wronk_resting_017_erm_raw.fif'
-fname_erm_save = '/media/Toshiba/Code/rsn_data/wronk_resting_017/raw_fif/wronk_resting_017_erm_short_raw.fif'
-fname_raw_eve_save = '/media/Toshiba/Code/rsn_data/wronk_resting_017/lists/ALL_wronk_resting_017_01-eve.lst'
-fname_erm_eve_save = '/media/Toshiba/Code/rsn_data/wronk_resting_017/lists/ALL_wronk_resting_017_erm_short-eve.lst'
+for subj_num in subj_nums:
+    full_name = 'wronk_resting_%03d' % subj_num
+    fname_raw = op.join(data_dir, full_name, 'raw_fif', full_name +
+                        '_01_raw.fif')
+    fname_raw_save = op.join(data_dir, full_name, 'raw_fif', full_name +
+                             '_01_short_raw.fif')
+    fname_erm = op.join(data_dir, full_name, 'raw_fif', full_name +
+                        '_erm_raw.fif')
+    fname_erm_save = op.join(data_dir, full_name, 'raw_fif', full_name +
+                             '_erm_short_raw.fif')
+    fname_raw_eve_save = op.join(data_dir, full_name, 'lists', 'ALL_' +
+                                 full_name + '_01-eve.lst')
+    fname_erm_eve_save = op.join(data_dir, full_name, 'lists', 'ALL_' +
+                                 full_name + '_erm-eve.lst')
 
-for fname, sname, eve_name in zip([fname_raw, fname_erm],
-                                  [fname_raw_save, fname_erm_save],
-                                  [fname_raw_eve_save, fname_erm_eve_save]):
-    raw = mne.io.Raw(fname, preload=True, verbose=True, allow_maxshield=True)
-    raw.crop(tmax=120, copy=False)
-    raw.save(sname, overwrite=True)
-    eve = mne.make_fixed_length_events(raw, 1)
-    mne.write_events(eve_name, eve)
-    '''
+    for fname, sname, eve_name in zip([fname_raw, fname_erm],
+                                      [fname_raw_save, fname_erm_save],
+                                      [fname_raw_eve_save, fname_erm_eve_save]):
+        raw = mne.io.Raw(fname, preload=True, verbose=True,
+                         allow_maxshield=True)
+        if debug:
+            raw.crop(tmax=120, copy=False)
+            raw.save(sname, overwrite=True)
+        eve = mne.make_fixed_length_events(raw, 1, duration=epo_len)
+        mne.write_events(eve_name, eve)
 
-# Setup cuda and set `n_jobs_resampe='cuda'`
+# TODO: Setup cuda and set `n_jobs_resampe='cuda'`
 # TODO check t_adjust (trigger delay)
-params = mnefun.Params(tmin=0., tmax=1., n_jobs=6, lp_cut=55, decim=6, proj_sfreq=200,
-                       n_jobs_mkl=6, n_jobs_fir=6, n_jobs_resample=6,
-                       filter_length='5s', hp_cut=1)
+params = mnefun.Params(tmin=0., tmax=epo_len, n_jobs=6, n_jobs_mkl=1,
+                       n_jobs_fir=6, n_jobs_resample=6,
+                       decim=5, proj_sfreq=200, filter_length='10s',
+                       cov_method='shrunk')
+
+params.subjects = subj_nums
+params.convert_subjects('wronk_resting_%03d', 'AKCLEE_1%02d')
+params.dates = [None] * len(params.subjects)  # don't fill in with actual date
+params.subject_indices = np.setdiff1d(np.arange(len(params.subjects)), [])
+params.ssp_ecg_reject = params.reject
 
 #TODO: check 1) filter_length, 2) hpcut, 3) cov_method 4) baselining
 
-params.subjects = ['wronk_resting_%s' % num for num in subj_nums]
-params.structurals = ['AKCLEE_1%s' % num[-2:] for num in subj_nums]  # Struct dirs
+#params.subjects = ['wronk_resting_%s' % num for num in subj_nums]
+#params.structurals = ['AKCLEE_1%s' % num[-2:] for num in subj_nums]  # Struct dirs
 params.work_dir = data_dir
-params.subject_indices = np.arange(len(subj_nums))
+#params.subject_indices = np.arange(len(subj_nums))
+
+params.sws_ssh = 'itwronk@kasga.ilabs.uw.edu'
+params.sws_dir = '/data06/itwronk/sss_work_dir'
+params.work_dir = data_dir
 
 params.baseline = None  # Don't apply baselineing
-#params.cov_method = 'emperical'
-params.cov_method = 'shrunk'  # How to calc covariance. 'emperical', 'shrunk',
 params.epochs_type = 'fif'
 
 #params.on_process = handler  # Fails trying to find *raw_sss.fif
 params.on_process = None
+params.plot_drop_logs = False
 
 # Run/save information
 params.analyses = ['All']
-params.in_names = ['start']     # Input event names
+params.in_names = ['rest']      # Input event names
 params.in_numbers = [1]         # Input event numbers
-params.out_names = [['start']]  # Output event names
+params.out_names = [['rest']]   # Output event names
 params.out_numbers = [[1]]      # Output types (all 0)
-#params.run_names = ['%s_01']    # Run names (all _01 here)
-#params.runs_empty = ['%s_erm']  # Name of empty room data
-params.run_names = ['%s_01_short']    # Run names (all _01 here)
-params.runs_empty = ['%s_erm_short']  # Name of empty room data
+params.run_names = ['%s_01']    # Run names (all _01 here)
+params.runs_empty = ['%s_erm']  # Name of empty room data
+#params.run_names = ['%s_01_short']    # Run names of shortened data (all _01 here)
+#params.runs_empty = ['%s_erm_short']  # Name of shortened empty room data
 params.get_projs_from = np.arange(len(params.run_names))
 params.proj_nums = [[1, 1, 0],  # ECG: grad/mag/eeg
-                    [1, 1, 2],  # EOG; XXX [1, 1, 2] fails for ERM data
+                    [1, 1, 2],  # EOG
                     [0, 0, 0]]  # Continuous (from ERM)
 
 # Fwd/Inv
 params.inv_runs = [np.arange(len(params.run_names))]
-params.inv_names = ['%s']
+#params.inv_names = ['%s']
 
 # SSS
 params.sss_type = 'python'  # Run python implementation of SSS
-# TODO: change back to 60 when done validating on 30s test data
-params.tsss_dur = 60.       # Default, tSSS windown duration
+params.tsss_dur = 10.       # Default, tSSS windown duration
 
 params.freeze()
 mnefun.do_processing(params,
-                     do_score=True,
-                     do_sss=True,        # Apply SSS filtering (locally)
-                     do_ch_fix=True,     # Fix channel ordering
-                     gen_ssp=True,       # Gen/Apply signal-space separation for artifacts
-                     apply_ssp=True,     # ^^^
-                     write_epochs=True,  # Save epochs objects
-                     gen_covs=True,      # Generate covariances from baseline (should be using ERM)
-                     gen_fwd=True,       # Generate forward matrix
-                     gen_inv=True,       # Generate inverse matrix
-                     gen_report=True,    # Write html of results
-                     print_status=True)
+                     do_score=False,      # F.
+                     do_sss=False,        # T. Apply SSS filtering (locally)
+                     do_ch_fix=False,     # ? Fix channel ordering
+                     gen_ssp=False,       # T. for voc_meg. Reuse pre-generated SSPs for resting data
+                     apply_ssp=True,      # T.
+                     write_epochs=True,   # T. Save epochs objects
+                     gen_covs=False,      # F. Generate covariances from baseline (should be using ERM)
+                     gen_fwd=False,       # T. Generate forward matrix
+                     gen_inv=False,       # T. Generate inverse matrix
+                     gen_report=True,     # T. Write html of results
+                     print_status=False)
