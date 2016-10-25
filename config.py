d47e7af68b97a232d62b2afed36775ce66aca0e0
wronk <wronk.mark@gmail.com>
2016-10-25 13:29:00 -0700

    Adding support for connectivity estimation in both hemispheres

--- config.py
+++ config.py
@@ -11,7 +11,12 @@ import numpy as np
 # divisions
 # XXX: Could test both left and right hemispheres instead of just one
 
-# Using M/EEG data
+# 019 had bounced 1 triggers in original experiment.
+# 020 had bad pupillometry, and the first run had a TTL issue
+# 007 had bad pupillometry; All epochs rejected
+# 022, 004 had droopy pupillometry
+subj_nums = [15, 17, 19, 23, 31, 32, 34, 38]
+inv_lambda = 1 / 9.
 
 # Divisions on fsaverage
 # 7 div1, ind0: MPFC (Medial frontal region)
@@ -23,8 +28,21 @@ import numpy as np
 # 7 div1, ind5: MT, SPL7A, etc. (Large region)
 # 7 div2, ind6: FEF, (frontal eye field)
 
-rsn_labels = ['lh.7Networks_7_div%i.label' % i for i in range(1, 6)] + \
+# Right half
+# 7 div1, ind7: MPFC (Medial frontal region)
+# 7 div2, ind8: STS (Superior temporal sulcus)
+# 7 div3, ind9: PCC, pCun (Posterior cing. cort., preCuneus)
+# 7 div4, ind10: TPJ, (temporoparietal junction) or angular gyrus(?)
+# 7 div5, ind11: XXX Name? Small region on medial/ventral side of temporal lobe
+
+# 7 div1, ind12: MT, SPL7A, etc. (Large region)
+# 7 div2, ind13: FEF, (frontal eye field)
+
+rsn_labels_lh = ['lh.7Networks_7_div%i.label' % i for i in range(1, 6)] + \
     ['lh.7Networks_3_div%i.label' % k for k in range(1, 3)]
+rsn_labels_rh = ['rh.7Networks_7_div%i.label' % i for i in range(1, 6)] + \
+    ['rh.7Networks_3_div%i.label' % k for k in range(1, 3)]
+rsn_labels = rsn_labels_lh + rsn_labels_rh
 
 # Define processing parameters
 # In preprocessing, frequency cutoff at 55 Hz
@@ -39,8 +57,8 @@ for adict in config_conn_methods:
     adict.update(common_params)
 
 # conn_pairs should index into RSN divisions on fsaverage
-config_conn_params = dict(conn_pairs=(np.array([3, 3, 3, 5]),
-                                      np.array([0, 1, 2, 6])),
+config_conn_params = dict(conn_pairs=(np.array([3, 3, 3, 5, 10, 10, 10, 12]),
+                                      np.array([0, 1, 2, 6, 7, 8, 9, 13])),
                           conn_methods=config_conn_methods,
                           rsn_labels=rsn_labels,
                           mean_mode='mean_flip',
