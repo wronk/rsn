a59ba64fc107eeb7a138a9af64bfb8c7c79fbc1d
wronk <wronk.mark@gmail.com>
2016-11-03 10:59:12 -0700

    Minor updates to how data is organized

--- plot_gridsearch_lr.py
+++ plot_gridsearch_lr.py
@@ -25,7 +25,7 @@ struct_dir = os.environ['SUBJECTS_DIR']
 data_head = op.join(os.environ['CODE_ROOT'])
 saved_data_dir = op.join(data_head, 'rsn_results')
 
-file_name = 'lr_scores_2016_10_26__11_56.pkl'
+file_name = 'lr_scores_2016_10_28__21_07.pkl'
 
 ###################
 # Load pickled data
@@ -34,7 +34,7 @@ fpath_data = op.join(saved_data_dir, file_name)
 with open(fpath_data) as pkl_file:
     result_dict = cPickle.load(pkl_file)
 
-print 'Loaded data (%s) from %s' % (fpath_data, result_dict['time_finished'])
+#print 'Loaded data (%s) from %s' % (fpath_data, result_dict['time_finished'])
 print 'Hyper param format: %s' % result_dict['hyper_param_desc']
 print 'Hyper param size: %s' % str(result_dict['class_scores'].shape)
 
@@ -52,7 +52,7 @@ hyp_p = result_dict['hyper_params']
 ###################
 plt.ion()
 
-fig1, axes1 = plt.subplots(cls_shape[0], cls_shape[1], figsize=(14, 6),
+fig1, axes1 = plt.subplots(cls_shape[0], cls_shape[1], figsize=(14, 6.5),
                            sharex=True, sharey=True)
 
 #vmin, vmax = np.min(cls_arr), np.max(cls_arr)
@@ -67,14 +67,14 @@ for ind_2d in list(product(*[range(cls_shape[0]), range(cls_shape[1])])):
 
     # Set ticks and labels to match L1/L2 loss
     axes1[ind_2d].set_xticks(range(cls_shape[3]))
-    axes1[ind_2d].set_xticklabels(hyp_p[1])
+    axes1[ind_2d].set_xticklabels(hyp_p[2], rotation=-45)
 
     axes1[ind_2d].set_yticks(range(cls_shape[2]))
-    axes1[ind_2d].set_yticklabels(hyp_p[0])
+    axes1[ind_2d].set_yticklabels(hyp_p[1])
 
     # Set batch
     if ind_2d[0] == 0:
-        title = 'Batch size: %i' % hyp_p[2][ind_2d[1]]
+        title = 'Batch size: %i' % hyp_p[0][ind_2d[1]]
         axes1[ind_2d].set_title(title)
     elif ind_2d[0] == len(result_dict['subj_nums']) - 1:
         axes1[ind_2d].set_xlabel('L2 loss weight')
