11e698abf87127ac25c58797eb5cb27a8cfc1a83
wronk <wronk.mark@gmail.com>
2017-02-21 18:06:27 -0800

    Streamline/functionalize epoching

--- comp_fun.py
+++ comp_fun.py
@@ -9,11 +9,15 @@ import os
 import os.path as op
 import numpy as np
 
+import mne
 from mne import read_epochs, concatenate_epochs
 from mne.time_frequency.tfr import _compute_tfr
+import hcp
+from hcp import preprocessing as preproc
+from hcp.preprocessing import interpolate_missing as interp_missing
 
 import config as cf
-from config import motor_params, rest_params, hcp_path
+from config import (motor_params, rest_params, hcp_path, filt_params)
 
 
 def tfr_split(data, processing_params):
@@ -80,11 +84,82 @@ def get_concat_epos(subject, exp_type):
     else:
         raise RuntimeError('Incorrect trial designation: %s' % exp_type)
 
+    # XXX: check if proj should be false
     epo_list = []
     for run_i in runs:
-        epo = read_epochs(op.join(hcp_path, '%s' % subject, 'epochs',
-                          '%s_%s_run%i-epo.fif' % (subject, exp_type, run_i)))
+        epo_fname = op.join(hcp_path, '%s' % subject, 'epochs',
+                            '%s_%s_run%i-epo.fif' % (subject, exp_type, run_i))
+        epo = read_epochs(epo_fname, proj=False)
         epo_list.append(epo)
 
     print '\nConcatenating %i epoch files' % len(epo_list)
     return concatenate_epochs(epo_list)
+
+
+def preproc_annot_filter(raw, hcp_params):
+    """Helper to annotate bad segments and apply Butterworth freq filtering"""
+
+    # apply ref channel correction and drop ref channels
+    preproc.apply_ref_correction(raw)
+
+    # construct MNE annotations
+    annots = hcp.read_annot(**hcp_params)
+    bad_seg = (annots['segments']['all']) / raw.info['sfreq']
+    annotations = mne.Annotations(
+        bad_seg[:, 0], (bad_seg[:, 1] - bad_seg[:, 0]),
+        description='bad')
+
+    raw.annotations = annotations
+    raw.info['bads'].extend(annots['channels']['all'])
+    raw.pick_types(meg=True, ref_meg=False)
+
+    # Note: MNE complains on Python 2.7
+    raw.filter(filt_params['lp'], filt_params['hp'],
+               method=filt_params['method'],
+               iir_params=filt_params['iir_params'], n_jobs=-1)
+
+    return raw, annots
+
+
+def preproc_artifacts(raw, hcp_params, annots):
+    """Helper to apply artifact removal on raw data"""
+    # Read ICA and remove EOG ECG
+    # Note that the HCP ICA assumes that bad channels have been removed
+    ica_mat = hcp.read_ica(**hcp_params)
+
+    # We will select the brain ICs only
+    #exclude = [ii for ii in range(annots['ica']['total_ic_number'][0])
+    #           if ii not in annots['ica']['brain_ic_vs']]
+    exclude = annots['ica']['ecg_eog_ic']
+
+    preproc.apply_ica_hcp(raw, ica_mat=ica_mat, exclude=exclude)
+
+    return raw
+
+
+def preproc_epoch(subj_fold, run_index, raw, events, exp_type):
+    """Helper to convert raw data into epochs"""
+
+    if exp_type == 'rest':
+        epo_params = rest_params
+    elif exp_type == 'task_motor':
+        epo_params = motor_params
+    else:
+        raise RuntimeError('exp_type must be `rest` or `task_motor`, got %s'
+                           % exp_type)
+
+    # Create and save epochs
+    #     Baseline?, XXX update rejects
+    events = np.sort(events, 0)
+    epochs = mne.Epochs(raw, events=events,
+                        event_id=epo_params['event_id'],
+                        tmin=epo_params['tmin'],
+                        tmax=epo_params['tmax'],
+                        reject=None, baseline=epo_params['baseline'],
+                        decim=epo_params['decim'], preload=True)
+
+    # Add back out channels for comparison across runs
+    epochs = interp_missing(epochs, subject=subj_fold,
+                            data_type=exp_type, run_index=run_index,
+                            hcp_path=hcp_path, mode='accurate')
+    return epochs
--- preproc/gen_motor_epochs.py
+++ preproc/gen_motor_epochs.py
@@ -1,18 +1,18 @@
 """
-gen_rest_epochs.py
+gen_motor_epochs.py
 
 @author: wronk
 
-Script to loop over HCP subjects, compute rest epochs
+Script to loop over HCP subjects and compute motor epochs
 """
 
 from os import path as op
+import numpy as np
 
-import mne
 import hcp
 
 from rsn import config as cf
-from rsn.config import hcp_path, rest_params
+from rsn.config import hcp_path, motor_params
 from rsn.comp_fun import (check_and_create_dir, preproc_annot_filter,
                           preproc_artifacts, preproc_epoch)
 
@@ -21,15 +21,14 @@ new_subjects_dir = op.join(hcp_path, 'anatomy')
 head_trans_dir = op.join(hcp_path, 'hcp-meg')
 
 n_jobs = 6
-exp_type = 'rest'
+exp_type = 'motor'
 
 # Get all subject ID numbers, exclude missing rest/motor/story&math/working mem
 dirs = cf.subj_nums_hcp
-dirs = list(set(dirs) - set(cf.hcp_subj_remove))
 dirs = [str(temp_d) for temp_d in dirs]
 dirs = dirs[0:1]
 
-hcp_params = dict(hcp_path=hcp_path, data_type=exp_type)
+hcp_params = dict(hcp_path=hcp_path, data_type='task_' + exp_type)
 
 for subj_fold in dirs:
     hcp_params['subject'] = subj_fold
@@ -38,18 +37,42 @@ for subj_fold in dirs:
     epo_fold = op.join(hcp_path, subj_fold, 'epochs')
     check_and_create_dir(epo_fold)
 
+    # Construct trial info for each run
+    # Note: need unprocessed data and preprocessed data (to get trial info
+    #       information in this step)
+    trial_infos = []
+    for run_index in motor_params['runs']:
+        hcp_params['run_index'] = run_index
+        trial_info = hcp.read_trial_info(**hcp_params)
+        trial_infos.append(trial_info)
+
+    print('\n' + 'Data info:' + '\n')
+    print(trial_info['stim']['comments'][:10])  # which column? # 3
+    #print(set(trial_info['stim']['codes'][:, 3]))  # check values
+
+    all_events = []
+    for trial_info in trial_infos:
+        events = np.c_[
+            trial_info['stim']['codes'][:, motor_params['time_samp_col']] - 1,
+            np.zeros(len(trial_info['stim']['codes'])),
+            trial_info['stim']['codes'][:, motor_params['stim_code_col']]
+        ].astype(int)
+
+        # Unfortunately, HCP data the time events may not always be unique
+        unique_subset = np.nonzero(np.r_[1, np.diff(events[:, 0])])[0]
+        events = events[unique_subset]  # use diff to find first unique events
+
+        all_events.append(events)
+
     ###########################################################################
     # Construct epochs
 
-    for run_index in rest_params['runs']:
+    for run_index, events in zip(motor_params['runs'], all_events):
 
         hcp_params['run_index'] = run_index
 
         raw = hcp.read_raw(**hcp_params)
         raw.load_data()
-
-        # Make 1 second events
-        events = mne.make_fixed_length_events(raw, id=1, duration=1.)
 
         #################
         # Preprocess data
@@ -61,7 +84,7 @@ for subj_fold in dirs:
         #################
         # Epoch data
         epochs = preproc_epoch(subj_fold, run_index, raw, events,
-                               exp_type=exp_type)
+                               exp_type='task_' + exp_type)
 
         #################
         # Save epochs
